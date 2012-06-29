7a7fb5a416226312dd3d509f54f9efb56119fe23
Cam Spiers <camspiers@gmail.com>
2012-06-29 15:11:50 +1200

    FEATURE: Unit testing evironment ready

---
+++ code/Heystack/Subsystem/Products/Tests/ProductHolderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Heystack\Subsystem\Products\Tests;
+
+class CurrencyTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    public function testA()
+    {
+        $this->assertEquals(true, true);
+    }
+
+}
---
+++ code/Heystack/Subsystem/Products/Tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+require_once dirname(dirname(dirname(dirname(dirname(__DIR__))))) . '/vendor/autoload.php';
