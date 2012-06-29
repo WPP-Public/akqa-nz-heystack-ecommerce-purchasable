42ba060f781662bccb71823db48f97bc1aa4ca37
Cam Spiers <camspiers@gmail.com>
2012-06-29 15:05:58 +1200

    BUGFIX: Remove duplicate director rule

--- _config.php
+++ _config.php
@@ -1,7 +1,3 @@
 <?php
 
 define('ECOMMERCE_PRODUCT_BASE_PATH', __DIR__);
-
-\Director::addRules(100, array(
-    \EcommerceInputController::$url_segment . '//$Action/$ID' => 'EcommerceInputController'
-));
