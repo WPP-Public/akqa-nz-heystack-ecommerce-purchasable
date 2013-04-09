91ac74a829002879778ad4a4eceadf1e56b9e9b3
Cam Spiers <camspiers@gmail.com>
2013-04-09 16:53:49 +1200

    FEATURE: New configuration for the container extension.

--- code/Heystack/Subsystem/Products/DependencyInjection/ContainerExtension.php
+++ code/Heystack/Subsystem/Products/DependencyInjection/ContainerExtension.php
@@ -10,13 +10,12 @@
  */
 namespace Heystack\Subsystem\Products\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
-
+use Heystack\Subsystem\Products\Config\ContainerConfig;
+use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-
-use Heystack\Subsystem\Core\DependencyInjection\ContainerExtensionConfigProcessor;
 
 /**
  * Container extension for Heystack.
@@ -26,23 +25,23 @@ use Heystack\Subsystem\Core\DependencyInjection\ContainerExtensionConfigProcesso
  * extension that loads heystacks services.yml
  *
  * @copyright  Heyday
- * @author Cam Spiers <cameron@heyday.co.nz>
- * @author Glenn Bautista <glenn@heyday.co.nz>
- * @package Ecommerce-Products
+ * @author     Cam Spiers <cameron@heyday.co.nz>
+ * @author     Glenn Bautista <glenn@heyday.co.nz>
+ * @package    Ecommerce-Products
  *
  */
-class ContainerExtension extends ContainerExtensionConfigProcessor implements ExtensionInterface
+class ContainerExtension extends Extension
 {
 
     /**
      * Loads a services.yml file into a fresh container, ready to me merged
      * back into the main container
      *
-     * @param  array            $config
+     * @param  array $configs
      * @param  ContainerBuilder $container
      * @return null
      */
-    public function load(array $config, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
 
         $loader = new YamlFileLoader(
@@ -52,32 +51,37 @@ class ContainerExtension extends ContainerExtensionConfigProcessor implements Ex
 
         $loader->load('services.yml');
 
-        $this->processConfig($config, $container);
-        
-        $config = array_pop($config);
-        
-        if(isset($config['yml.product']) && $container->hasDefinition('product_schema')){
-            
+        $config = (new Processor())->processConfiguration(
+            new ContainerConfig(),
+            $configs
+        );
+
+        if (isset($config['product_class'])) {
+            $container->setParameter('product.class', $config['product_class']);
+        }
+
+        if (isset($config['yml_product']) && $container->hasDefinition('product_schema')) {
+
             $definition = $container->getDefinition('product_schema');
-            
-            $definition->replaceArgument(0, $config['yml.product']);
-            
+
+            $definition->replaceArgument(0, $config['yml_product']);
+
         }
-        
-        if(isset($config['yml.productholder']) && $container->hasDefinition('product_holder_schema')){
-            
+
+        if (isset($config['yml_productholder']) && $container->hasDefinition('product_holder_schema')) {
+
             $definition = $container->getDefinition('product_holder_schema');
-            
-            $definition->replaceArgument(0, $config['yml.productholder']);
-            
+
+            $definition->replaceArgument(0, $config['yml_productholder']);
+
         }
-        
-        if(isset($config['yml.transaction_productholder']) && $container->hasDefinition('transaction_product_holder_schema')){
-            
+
+        if (isset($config['yml_transaction_productholder']) && $container->hasDefinition('transaction_product_holder_schema')) {
+
             $definition = $container->getDefinition('transaction_product_holder_schema');
-            
-            $definition->replaceArgument(0, $config['yml.transaction_productholder']);
-            
+
+            $definition->replaceArgument(0, $config['yml_transaction_productholder']);
+
         }
 
     }
